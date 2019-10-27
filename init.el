b6760a11491115b5feca7e19ce193d6a9a78569f
mwgkgk <mwgkgk@gmail.com>
2019-10-27 11:15:51 +0200

    Move colors to layers/colors.el

--- init.el
+++ init.el
@@ -86,6 +86,7 @@
   ;; Use `prescient' for Ivy menus.
   (ivy-prescient-mode 1))
 
+
 ;;; ----------
 ;;; Completion
 ;;; ----------
@@ -248,16 +249,4 @@
 ;;; Themes
 ;;; ------
 
-(setq current-theme 'dichromacy)
-
-(load-theme current-theme t)
-
-(defun toggle-theme ()
-  (interactive)
-  (if (equal current-theme 'dichromacy)
-    (progn (setq current-theme 'tango-dark)
-           (load-theme current-theme t))
-    (progn (setq current-theme 'dichromacy)
-           (load-theme current-theme t))))
-
-(define-key evil-normal-state-map (kbd ";bg") 'toggle-theme)
+(load "~/.emacs.d/layers/colors.el")
---
+++ layers/colors.el
@@ -0,0 +1,13 @@
+(setq current-theme 'dichromacy)
+
+(load-theme current-theme t)
+
+(defun toggle-theme ()
+  (interactive)
+  (if (equal current-theme 'dichromacy)
+    (progn (setq current-theme 'tango-dark)
+           (load-theme current-theme t))
+    (progn (setq current-theme 'dichromacy)
+           (load-theme current-theme t))))
+
+(define-key evil-normal-state-map (kbd ";bg") 'toggle-theme)
