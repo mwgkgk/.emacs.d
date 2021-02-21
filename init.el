42a34c370c5d3e4f58e4279bea267fac74726b9b
mwgkgk <mwgkgk@gmail.com>
2021-02-22 00:03:14 +0200

    (define-key evil-normal-state-map (kbd ";U") 'yas-visit-snippet-file)

--- init.el
+++ init.el
@@ -176,7 +176,8 @@
 (use-package yasnippet
   :init
   (yas-global-mode 1)
-  (define-key evil-normal-state-map (kbd ";u") 'yas-new-snippet))
+  (define-key evil-normal-state-map (kbd ";u") 'yas-new-snippet)
+  (define-key evil-normal-state-map (kbd ";U") 'yas-visit-snippet-file))
 
 ;;; ------
 ;;; Parens
