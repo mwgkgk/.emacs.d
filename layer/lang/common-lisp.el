759ead9d79fdbff5df3cf5e9df6b24cf12f5833a
mwgkgk <mwgkgk@gmail.com>
2021-04-03 13:11:00 +0300

    Sly config

--- layer/lang/common-lisp.el
+++ layer/lang/common-lisp.el
@@ -1,4 +1,12 @@
 ;;; Fri Apr  2 10:51:56 UTC 2021 ;;; 
 
-(use-package sly)
+(use-package sly
+  :init
+  (setq inferior-lisp-program "/usr/bin/sbcl")
+
+  :config
+  (add-hook 'sly-mode-hook
+               (lambda ()
+                 (unless (sly-connected-p)
+                   (save-excursion (sly))))))
 
