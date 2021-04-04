d62fc739ecf2ae71a7d4c21bceed44359b882dd6
mwgkgk <mwgkgk@gmail.com>
2021-04-04 10:52:07 +0300

    More fzf binds

--- keys/prefix/s.el
+++ keys/prefix/s.el
@@ -10,8 +10,42 @@
 ;;; Fri Mar 19 04:44:25 UTC 2021 ;;; 
 
 ;;; Goes to project root by default
-(define-key evil-normal-state-map (kbd "s s") 'fzf)
+(define-key evil-normal-state-map (kbd "ss")
+  #'(lambda () (interactive) (fzf/start projectile-project-root)))
 
 ;;; Sun Apr  4 00:28:08 UTC 2021 ;;;
 
-(define-key evil-normal-state-map (kbd "se") '(lambda () (interactive) (find-file ".")))
+(define-key evil-normal-state-map (kbd "se")
+  #'(lambda () (interactive) (fzf/start default-directory)))
+
+;;; Sun Apr  4 04:35:51 UTC 2021 ;;; 
+
+(define-key evil-normal-state-map (kbd "scc")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/")))
+(define-key evil-normal-state-map (kbd "sck")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/keys/")))
+(define-key evil-normal-state-map (kbd "scu")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/snippets/")))
+(define-key evil-normal-state-map (kbd "sca")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/api/")))
+(define-key evil-normal-state-map (kbd "scr")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/layer/")))
+(define-key evil-normal-state-map (kbd "scl")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/lib/")))
+(define-key evil-normal-state-map (kbd "scp")
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/straight/repos/")))
+
+(define-key evil-normal-state-map (kbd "sD")
+  #'(lambda () (interactive) (fzf/start "~/dot/")))
+
+;;; Sun Apr  4 05:23:18 UTC 2021 ;;; 
+
+(define-key evil-normal-state-map (kbd "sr")
+  #'(lambda ()
+      (interactive)
+      (fzf/start (file-name-directory (directory-file-name default-directory)))))
+
+;;; Sun Apr  4 06:04:33 UTC 2021 ;;; 
+
+(define-key evil-normal-state-map (kbd "sol") #'load-theme)
+
