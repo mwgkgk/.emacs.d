729cf17e44a2002a0759eb3d4449bc8e56bb4faf
mwgkgk <mwgkgk@gmail.com>
2021-04-04 06:24:42 +0300

    (load "~/.emacs.d/lib/evil/api/i/comfy-eval.el")

--- keys/ft/elisp.el
+++ keys/ft/elisp.el
@@ -1,3 +1,7 @@
+;;; Sun Apr  4 03:23:37 UTC 2021 ;;; 
+
+(load "~/.emacs.d/lib/evil/api/i/comfy-eval.el")
+
 (add-hook 'emacs-lisp-mode-hook
     '(lambda ()
        (define-key evil-normal-state-map (kbd "X") 'lib/evil/i:comfy-eval-x)
