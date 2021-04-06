f4e859db11d76f3e789b30d3040335f3f50ff8f3
mwgkgk <mwgkgk@gmail.com>
2021-04-06 12:22:32 +0300

    (global-set-key (kbd "C-h TAB") #'info)

---
+++ keys/prefix/c-h.el
@@ -0,0 +1,5 @@
+;;; Tue Apr  6 08:41:18 UTC 2021 ;;; 
+
+;;; Aka fuck my life when I press C-h i wrong:
+;;; But also kinda funny to use on its own god bless.
+(global-set-key (kbd "C-h TAB") #'info)
