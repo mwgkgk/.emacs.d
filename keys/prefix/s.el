187861c2d37bc5dd458f04ae34bce3d072f28459
mwgkgk <mwgkgk@gmail.com>
2022-08-02 12:44:39 +0000

    Fix fzf

--- keys/prefix/s.el
+++ keys/prefix/s.el
@@ -22,34 +22,34 @@
 ;;; Sun Apr  4 00:28:08 UTC 2021 ;;;
 
 (define-key evil-normal-state-map (kbd "se")
-  #'(lambda () (interactive) (fzf/start default-directory)))
+  #'(lambda () (interactive) (fzf/start default-directory #'fzf/action-find-file)))
 
 ;;; Sun Apr  4 04:35:51 UTC 2021 ;;; 
 
 (define-key evil-normal-state-map (kbd "sd")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/" #'fzf/action-find-file)))
 (define-key evil-normal-state-map (kbd "sck")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/keys/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/keys/" #'fzf/action-find-file)))
 (define-key evil-normal-state-map (kbd "scu")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/snippets/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/snippets/" #'fzf/action-find-file)))
 (define-key evil-normal-state-map (kbd "sca")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/api/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/api/" #'fzf/action-find-file)))
 (define-key evil-normal-state-map (kbd "scr")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/layer/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/layer/" #'fzf/action-find-file)))
 (define-key evil-normal-state-map (kbd "scl")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/lib/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/lib/" #'fzf/action-find-file)))
 (define-key evil-normal-state-map (kbd "scp")
-  #'(lambda () (interactive) (fzf/start "~/.emacs.d/straight/repos/")))
+  #'(lambda () (interactive) (fzf/start "~/.emacs.d/straight/repos/" #'fzf/action-find-file)))
 
 (define-key evil-normal-state-map (kbd "sD")
-  #'(lambda () (interactive) (fzf/start "~/dot/")))
+  #'(lambda () (interactive) (fzf/start "~/dot/" #'fzf/action-find-file)))
 
 ;;; Sun Apr  4 05:23:18 UTC 2021 ;;; 
 
 (define-key evil-normal-state-map (kbd "sr")
   #'(lambda ()
       (interactive)
-      (fzf/start (file-name-directory (directory-file-name default-directory)))))
+      (fzf/start (file-name-directory (directory-file-name default-directory)) #'fzf/action-find-file)))
 
 ;;; Sun Apr  4 06:04:33 UTC 2021 ;;; 
 
@@ -62,5 +62,5 @@
 ;;; Mon Apr  5 02:26:47 UTC 2021 ;;; 
 
 (define-key evil-normal-state-map (kbd "sx")
-  #'(lambda () (interactive) (fzf/start "~/org/")))
+  #'(lambda () (interactive) (fzf/start "~/org/" #'fzf/action-find-file))))))
 
