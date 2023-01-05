f9ab27b36c346f8f081553cb22272523ccfae048
mwgkgk <mwgkgk@gmail.com>
2023-01-05 06:14:32 +0200

    (set-face-attribute 'default nil :font "Hack" :height 67)

--- layer/interface.el
+++ layer/interface.el
@@ -18,8 +18,8 @@
 ;;; ===== ;;;
 
 ;; (set-face-attribute 'default nil :font "Iosevka SS04" :height 75)
-;; (set-face-attribute 'default nil :font "Hack" :height 77)
-(set-face-attribute 'default nil :font "Anka/Coder" :height 109)
+(set-face-attribute 'default nil :font "Hack" :height 67)
+;; (set-face-attribute 'default nil :font "Anka/Coder" :height 109)
 ;; (set-face-attribute 'default nil :font "Anka/Coder Narrow" :height 95)
 ;; (set-face-attribute 'default nil :font "scientifica")
 ;; (set-face-attribute 'default nil :font "Monospace" :height 150)
