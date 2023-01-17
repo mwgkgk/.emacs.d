6d0ca451d4cacf89f02b02ea8a34bb501429d952
mwgkgk <mwgkgk>
2023-01-17 04:19:39 +0200

    Font size

--- layer/interface.el
+++ layer/interface.el
@@ -18,7 +18,8 @@
 ;;; ===== ;;;
 
 ;; (set-face-attribute 'default nil :font "Iosevka SS04" :height 75)
-(set-face-attribute 'default nil :font "Hack" :height 67)
+;; (set-face-attribute 'default nil :font "Hack" :height 67)
+(set-face-attribute 'default nil :font "Hack" :height 109)
 ;; (set-face-attribute 'default nil :font "Anka/Coder" :height 109)
 ;; (set-face-attribute 'default nil :font "Anka/Coder Narrow" :height 95)
 ;; (set-face-attribute 'default nil :font "scientifica")
