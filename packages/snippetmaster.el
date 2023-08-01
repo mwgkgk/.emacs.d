e72c5732df51d72d6aaec7c6f0ca34190a3dbfac
mwgkgk <mwgkgk@gmail.com>
2023-08-01 16:54:43 +0300

    Venture on the road of slightly improving snippets

--- packages/snippetmaster.el
+++ packages/snippetmaster.el
@@ -1,9 +1,16 @@
-;;; Wed Jan 25 13:34:52 UTC 2023
+;;; TODO change yas-expand key to this wrapper
 
-(defparamenter *hippy:snippet-state* nil)
-
-(defun hippy:summon-snippet ()
+(defun death-by-snip-snip ()
+  "Heuristics for tab expansion."
   (interactive)
-  ;; The idea is as follows:
-  ;; Get line
-  nil)
+  (let ((word (thing-at-point 'symbol t))
+	(bounds (bounds-of-thing-at-point 'symbol)))
+    (cond
+     ;;; Instead of this, a propper context-aware snippet engine
+     ((member word (yas-active-keys)) (yas-expand))
+     ;;; Instead of this, N fallback heuristics
+     (t (progn
+	  (delete-region (car bounds) (cdr bounds))
+	  (insert (format "(%s)" word)))))))
+
+(define-key evil-insert-state-map (kbd "C-c e") #'death-by-snip-snip)
