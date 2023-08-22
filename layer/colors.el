5d0e9731d1a6a5ed3d3d92f7cb57af0a33778b36
mwgkgk <mwgkgk>
2023-08-22 10:27:35 +0300

    Passing t as second arg suppresses warning for checksum

--- layer/colors.el
+++ layer/colors.el
@@ -1,24 +1,4 @@
-;;; Sat Apr  3 09:28:23 UTC 2021 ;;; 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("9a9b10b0a409cf8816ca53428c0f87179a0a01edf2d334f85d71b4e9381bba80"
-     "9b39b25c3a23b1be6e99a3648b91ebaf2a7efdde236e3472aa95f1708ec61d4f"
-     "efd849c804148b88536914ccdee08285fd7376e2e3334522c9afc00fd7e594da"
-     "94146ac747852749e9444b184eb1e958f0e546072f66743929a05c3af62de473"
-     "6198e96f1fd7de3889a1b6ab8be1fc9b7c734cc9db0b0f16b635a2974601f977"
-     default)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; Tue Aug 22 07:21:34 UTC 2023
 
 ;;; Dark
 (use-package soothe-theme)
@@ -32,4 +12,5 @@
 (use-package goose-theme)
 (use-package hydandata-light-theme)
 
-(load-theme 'exotica t)
+;;; Passing t as second arg suppresses warning for checksum
+(load-theme 'dracula t)
